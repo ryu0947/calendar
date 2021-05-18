79a703b021ad762a12ce3244f2e2bf8f7d7265a0
ryu <dragonogawaz@gmail.com>
2021-05-18 21:29:32 +0900

    1ヶ月の日数を取得

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 "use strict";
 
 {
-  const year = 2020;
+  const year = 2021;
   const month = 4; // 5月
+
+  function getCalendarBody() {
+    const dates = [];
+    // 翌月の0日目と引数に設定すると今月の末日を取得できる
+    const lastDate = new Date(year, month + 1, 0).getDate();
+
+    for (let i = 1; i <= lastDate; i++) {
+      dates.push(i);
+    }
+
+    console.log(dates);
+  }
+
+  getCalendarBody();
 }
