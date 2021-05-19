841d07c4a2e3a64e81fb95526237cdc402225bb4
ryu <dragonogawaz@gmail.com>
2021-05-19 19:06:06 +0900

    前月の日数をカレンダーに表示するための関数を追加

--- js/main.js
+++ js/main.js
@@ -4,6 +4,27 @@
   const year = 2021;
   const month = 4; // 5月
 
+  function getCalendarHead() {
+    const calenderHead = [];
+    // 前月の末日
+    const d = new Date(year, month, 0).getDate();
+    // 今月の1日の曜日
+    const n = new Date(year, month, 1).getDay();
+
+    console.log(d);
+    console.log(n);
+
+    for (let i = 0; i < n; i++) {
+      calenderHead.unshift({
+        date: d - i,
+        isTody: false,
+        isDisable: false,
+      });
+    }
+
+    console.log(calenderHead);
+  }
+
   function getCalendarBody() {
     const dates = [];
     // 翌月の0日目と引数に設定すると今月の末日を取得できる
@@ -11,14 +32,15 @@
 
     for (let i = 1; i <= lastDate; i++) {
       dates.push({
-          date: i,
-          isTody: false,
-          isDisable: false
+        date: i,
+        isTody: false,
+        isDisable: false,
       });
     }
 
     console.log(dates);
   }
 
-  getCalendarBody();
+  getCalendarHead();
+  //   getCalendarBody();
 }
