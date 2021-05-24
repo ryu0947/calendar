93e9926909470ea66e70e4123010615f030a08e0
ryu <dragonogawaz@gmail.com>
2021-05-25 07:58:56 +0900

    カレンダーの日付を取得

--- js/main2.js
+++ js/main2.js
@@ -1,5 +1,26 @@
 "use strict";
 
 {
-  
+  const date = new Date();
+  const year = date.getUTCFullYear();
+  const month = date.getMonth();
+
+  function calenderBody() {
+    const dates = [];
+    const lastDay = new Date(year, month - 1, 0).getDate();
+
+    for (let i = 1; i < lastDay + 1; i++) {
+      dates.push({
+        date: i,
+        today: false,
+        disable: false,
+      });
+    }
+
+    if (year === date.getFullYear() && month === date.getMonth()) {
+      dates[date.getDate() - 1].today = true;
+    }
+  }
+
+  calenderBody();
 }
